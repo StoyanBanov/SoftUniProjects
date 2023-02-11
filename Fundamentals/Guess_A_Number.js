b5aa0de02951bae46a78a6811a37a4290fcc90bc
StoyanBanov <sbanov05@gmail.com>
2023-02-11 17:05:08 +0200

    Create Guess_A_Number.js

---
+++ Fundamentals/Guess_A_Number.js
@@ -0,0 +1,36 @@
+function guessANumber() {
+    const readline = require('readLine').createInterface({
+        input: process.stdin,
+        output: process.stdout
+    })
+    
+    let numberToGuess = Math.floor(Math.random() * 100)
+    
+    let borderLow = 0
+    let borderHigh = 100
+    
+    let recursiveReadLineAsync = () => {
+        readline.question('Guess a number between (0-100): ', number => {
+            if(number >= borderLow && number <= borderHigh){
+                if(number > numberToGuess){
+                    borderHigh = number
+                    console.log('Lower!');
+                    recursiveReadLineAsync()
+                }else if(number < numberToGuess){
+                    borderLow = number
+                    console.log('Higher!');
+                    recursiveReadLineAsync()
+                }else{
+                    console.log('You guessed it!');
+                    readline.close()
+                }
+            }else{
+                console.log('Invalid input! Try again.');
+                recursiveReadLineAsync()
+            }
+        })
+    }
+    recursiveReadLineAsync()
+}
+
+guessANumber()
